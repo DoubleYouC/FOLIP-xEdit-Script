dbdc700122c36d0ecac85a322dd73194ad9359e8
DoubleYouC <66910033+DoubleYouC@users.noreply.github.com>
2026-07-29 18:22:13 -0400

    Find erasure

--- FOLIP.pas
+++ FOLIP.pas
@@ -25,7 +25,7 @@ var
 
     iFolipMasterFile, iFolipPluginFile, iCurrentPlugin: IwbFile;
 
-    uiScale, iSleepTime, dummyIndex: integer;
+    uiScale, iSleepTime: integer;
 
     sFolipPluginFileName, sFolipAfterGenerationPluginFileName, sEnableParentFormidExclusions, sIgnoredWorldspaces, sIgnoredPlugins, pluginFileNameHere: string;
 
@@ -184,7 +184,6 @@ begin
         bDeepScan := False;
         bPreviousBeforeGenerationPresent := False;
         SetJDOLineBreak(#13#10);
-        dummyIndex := 0;
 
         if ResourceExists(sUserSettingsFileName) then begin
             AddMessage('Loading user settings from ' + sUserSettingsFileName);
@@ -1487,7 +1486,7 @@ begin
             if not ElementExists(sub, 'BNAM - Original Material') then continue;
             originalMat := LowerCase(GetElementEditValues(sub, 'BNAM - Original Material'));
             if originalMat = '' then continue;
-            if slSubstitutions.Find(originalMat, dummyIndex) then begin
+            if slSubstitutions.IndexOf(originalMat) <> -1 then begin
                 //duplicate found
                 Remove(sub);
                 Inc(count);
@@ -1887,7 +1886,7 @@ begin
             if GetIsCleanDeleted(r) then continue;
 
             base := WinningOverride(BaseRecord(r));
-            bBaseHasLOD := slHasLOD.Find(RecordFormIdFileId(base), dummyIndex);
+            bBaseHasLOD := slHasLOD.IndexOf(RecordFormIdFileId(base)) <> -1;
 
             //Split between refs with LOD and not having LOD
             if not bBaseHasLOD then begin
@@ -1974,7 +1973,7 @@ begin
                 r := ObjectToElement(tlOppositeEnableRefs[oi]);
 
                 OverOrNew := 'Overrides';
-                if slFakeStatics.Find(RecordFormIdFileId(BaseRecord(r)), dummyIndex) then OverOrNew := 'New';
+                if slFakeStatics.IndexOf(RecordFormIdFileId(BaseRecord(r))) <> -1 then OverOrNew := 'New';
 
                 rCell := LinksTo(ElementByIndex(r, 0));
                 if IsInteriorCell(rCell) then continue;
@@ -2075,7 +2074,7 @@ begin
                 then bIsFullLOD := true else bIsFullLOD := false;
 
                 OverOrNew := 'Overrides';
-                if slFakeStatics.Find(RecordFormIdFileId(BaseRecord(r)), dummyIndex) then OverOrNew := 'New';
+                if slFakeStatics.IndexOf(RecordFormIdFileId(BaseRecord(r))) <> -1 then OverOrNew := 'New';
 
 
                 if (not bCanBeRespected) or (not GetIsVisibleWhenDistant(r)) or bIsFullLOD then begin
@@ -2819,8 +2818,8 @@ begin
 
                         for oms := 0 to Pred(slLODOriginals.Count) do begin
                             om := slLODOriginals[oms];
-                            if slLODSubOriginal.Find(om, dummyIndex) then continue;
-                            if slExistingSubstitutions.Find('materials\' + om, dummyIndex) then continue;
+                            if slLODSubOriginal.IndexOf(om) <> -1 then continue;
+                            if slExistingSubstitutions.IndexOf('materials\' + om) <> -1 then continue;
                             rm := slLODReplacements[0];
                             if om = rm then continue;
                             slLODSubOriginal.Add(om);
@@ -2879,7 +2878,7 @@ begin
             if not ElementExists(sub, 'BNAM - Original Material') then continue;
             originalMat := LowerCase(GetElementEditValues(sub, 'BNAM - Original Material'));
             if originalMat = '' then continue;
-            if slSubstitutions.Find(originalMat, dummyIndex) then begin
+            if slSubstitutions.IndexOf(originalMat) <> -1 then begin
                 iCurrentPlugin := CanOverrideDeterminesPlugin(m, iFolipMasterFile);
                 iCurrentPlugin := RefMastersDeterminePlugin(m, iCurrentPlugin);
                 recordId := RecordFormIdFileId(m);
@@ -3013,7 +3012,7 @@ begin
 
         //Check if the replacementDiffuse already has a lod texture being created by TexGen.
         if not bForceTexGenRedo then bLodTextureExists := DoesTexGenAlreadyHaveTexture(replacementLodDiffuse) else bLodTextureExists := False;
-        bLodTextureExists := slAddedTexGenTextures.Find(replacementDiffuseNormalized, dummyIndex);
+        bLodTextureExists := slAddedTexGenTextures.IndexOf(replacementDiffuseNormalized) <> -1;
 
         //Attempt to add texgen rules to create the new lod replacement textures.
         if not bLodTextureExists then begin
@@ -3092,7 +3091,7 @@ begin
     diffuseNew := Fallback(joRasterizeMaterials.O[rm].S['RasterizedDiffusePath'], diffuseNew);
     Result := diffuseNew;
     outputTexture := sOutputDir + '\' + TrimLeftChars(diffuseNew, 4) + '.dds';
-    if slRasterizedDiffuses.Find(outputTexture, dummyIndex) then Exit;
+    if slRasterizedDiffuses.IndexOf(outputTexture) <> -1 then Exit;
     if FileExists(outputTexture) then Exit;
     if not bForceRasterize then if ResourceExists(diffuseNew) then if GrayscalePaletteTexturesVanilla(paletteTexture, replacementDiffuseNormalized) then Exit;
     slRasterizedDiffuses.Add(outputTexture);
@@ -3802,7 +3801,7 @@ begin
     if (Signature(s) = 'SCOL') and (cnt > 0) then begin
         bHasSCOLNeedingLOD := true;
         recordId := RecordFormIdFileId(s);
-        if not slHasLOD.Find(recordId, dummyIndex) then slHasLOD.Add(recordId);
+        if slHasLOD.IndexOf(recordId) = -1 then slHasLOD.Add(recordId);
         //check for base material swap on the SCOL record
         if ElementExists(r, 'Model\MODS - Material Swap') then begin
             ms := LinksTo(ElementByPath(r, 'Model\MODS'));
@@ -4056,7 +4055,7 @@ begin
                     slMatFiles.Add(LowerCase(f));
 
                     fNoLod := StringReplace(f, '\lod\', '\', [rfReplaceAll, rfIgnoreCase]);
-                    if (slVanilla.Find(LowerCase(fNoLod), dummyIndex) and slModded.Find(LowerCase(fNoLod), dummyIndex)) then begin
+                    if ((slVanilla.IndexOf(LowerCase(fNoLod)) <> -1) and (slModded.IndexOf(LowerCase(fNoLod)) <> -1)) then begin
                         if ((not ContainsText(LowerCase(fNoLod),'materials\architecture\shacks\shacklod01.bgsm')) and bMakeMissingMaterials) then begin
                             CompareModdedMaterialToVanilla(fNoLod, f);
                             bRasterized := true;
@@ -4136,7 +4135,7 @@ begin
         ResourceCount(f, slCurrentContainers);
         for i := Pred(slCurrentContainers.Count) downto 0 do begin
             currentContainer := slCurrentContainers[i];
-            if slVanillaContainers.Find(currentContainer, dummyIndex) then begin
+            if slVanillaContainers.IndexOf(currentContainer) <> -1 then begin
                 Result := currentContainer;
                 break;
             end;
@@ -4747,7 +4746,7 @@ begin
     if ruleOverride then hasDistantLOD := 0;
 
     model := LowerCase(wbNormalizeResourceName(model, resMesh));
-    if ((hasDistantLOD <> 0) and (not slCheckedModels.Find(model, dummyIndex))) then begin
+    if ((hasDistantLOD <> 0) and (slCheckedModels.IndexOf(model) = -1)) then begin
         slCheckedModels.Add(model);
 
         slMaterialsFromFullModel := TStringList.Create;
@@ -4792,7 +4791,7 @@ begin
 
     slPossibleLODPaths := TStringList.Create;
     try
-        if not slUsedLODNifFiles.Find(LODModel, dummyIndex) then begin
+        if slUsedLODNifFiles.IndexOf(LODModel) = -1 then begin
             slUsedLODNifFiles.Add(LODModel);
             if bReportNonLODMaterials or bReportUVs then begin
                 if MeshCheck(LODModel, slMaterialsFromLODModel) then slOutsideUVRange.Add('Warning: ' + LODModel + #9 + ' has UVs outside the 0 to 1 range.');
@@ -4817,7 +4816,7 @@ begin
                 //Check the LOD materials for matches in slPossibleLODPaths and warn if any are not matches.
                 for c := 0 to Pred(slMaterialsFromLODModel.Count) do begin
                     material := TrimRightChars(slMaterialsFromLODModel[c], 10);
-                    if not slPossibleLODPaths.Find(material, dummyIndex) then begin
+                    if slPossibleLODPaths.IndexOf(material) = -1 then begin
                         slMismatchedFullModelToLODMaterials.Add('Warning: ' + LODModel + #9 + ' has a LOD material that does not match the known full model materials: ' + #9 + material);
                     end;
                 end;
@@ -5033,7 +5032,7 @@ begin
         if bColorRemap then begin
             //p3 is for specific color remap level lod like 'model_0.5_lod_1.nif'
             p3 := searchModel + colorRemap + '_lod_' + level + '.nif';
-            if slNifFiles.Find(p3, dummyIndex) then begin
+            if slNifFiles.IndexOf(p3) <> -1 then begin
                 Result := TrimRightChars(p3, 7);
                 Exit;
             end;
@@ -5041,7 +5040,7 @@ begin
 
         //p1 is for specific level lod like 'model_lod_1.nif'
         p1 := searchModel + '_lod_' + level + '.nif';
-        if slNifFiles.Find(p1, dummyIndex) then begin
+        if slNifFiles.IndexOf(p1) <> -1 then begin
             if bColorRemap then begin
                 if bMakeMissingMaterials then begin
                     if not AttemptCreateColorRemapLODModel(p1, p3, colorRemap)
@@ -5063,7 +5062,7 @@ begin
 
         //p2 is for non-specific level lod like 'model_lod.nif'
         p2 := searchModel + '_lod.nif';
-        if (SameText(level, '0') and slNifFiles.Find(p2, dummyIndex)) then begin
+        if (SameText(level, '0') and (slNifFiles.IndexOf(p2) <> -1)) then begin
             Result := TrimRightChars(p2, 7);
             joModelMatch.O[fileNameStripped].S['lod' + level] := Result;
             Exit;
@@ -5122,7 +5121,7 @@ begin
                     GetBaseNameFromModel('meshes\' + Result, slLodModelNames);
                     c := 0;
                     for i := 0 to Pred(slLodModelNames.Count) do begin
-                        if not slModelNames.Find(slLodModelNames[i], dummyIndex) then continue;
+                        if slModelNames.IndexOf(slLodModelNames[i]) = -1 then continue;
                         Inc(c);
                     end;
                     if c = 0 then begin
@@ -5280,15 +5279,15 @@ begin
         searchMaterial := StringReplace(material, 'materials\' + slTopPaths[i], 'materials\' + slTopPaths[i] + 'lod\', [rfReplaceAll, rfIgnoreCase]);
         p1 := TrimLeftChars(searchMaterial, 5) + colorRemap + '.bgsm';
         //if Result = '' then Result := p1;
-        if (slMatFiles.Find(p1, dummyIndex) and not slExistingSubstitutions.Find(p1, dummyIndex)) then begin
+        if (slMatFiles.IndexOf(p1) <> -1 and slExistingSubstitutions.IndexOf(p1) = -1) then begin
             //sanity check
             //AddMessage(TrimRightChars(p1, 10));
             slPossibleLODPaths.Add(TrimRightChars(p1, 10));
         end;
         p2 := TrimLeftChars(searchMaterial, 5) + 'lod.bgsm';
-        if (slMatFiles.Find(p2, dummyIndex) and not slExistingSubstitutions.Find(p2, dummyIndex)) then slPossibleLODPaths.Add(TrimRightChars(p2, 10));
+        if (slMatFiles.IndexOf(p2) <> -1 and slExistingSubstitutions.IndexOf(p2) = -1) then slPossibleLODPaths.Add(TrimRightChars(p2, 10));
         p3 := TrimLeftChars(searchMaterial, 5) + '_lod.bgsm';
-        if (slMatFiles.Find(p3, dummyIndex) and not slExistingSubstitutions.Find(p3, dummyIndex)) then slPossibleLODPaths.Add(TrimRightChars(p3, 10));
+        if (slMatFiles.IndexOf(p3) <> -1 and slExistingSubstitutions.IndexOf(p3) = -1) then slPossibleLODPaths.Add(TrimRightChars(p3, 10));
     end;
 
     //Material Swap Map Rules
@@ -5296,7 +5295,7 @@ begin
     if joMswpMap.Contains(searchMaterial) then begin
         for a := 0 to Pred(joMswpMap.A[searchMaterial].Count) do begin
             p4 := joMswpMap.A[searchMaterial].S[a];
-            if not slExistingSubstitutions.Find('materials\' + p4, dummyIndex) then slPossibleLODPaths.Add(p4);
+            if slExistingSubstitutions.IndexOf('materials\' + p4) = -1 then slPossibleLODPaths.Add(p4);
         end;
     end;
 end;
@@ -5316,7 +5315,7 @@ begin
 
         for i := 0 to Pred(slMasters.Count) do begin
             if SameText(sFolipMasterFileName, slMasters[i]) then continue;
-            if not slMasterableMasters.Find(slMasters[i], dummyIndex) then begin
+            if slMasterableMasters.IndexOf(slMasters[i]) = -1 then begin
                 Result := iFolipPluginFile;
                 break;
             end;
@@ -5459,7 +5458,7 @@ begin
                 Result := True;
                 if i = 2 then begin
                     between := LowerCase(parts[1]) + '\';
-                    if not slTopLevelModPatternPaths.Find(between, dummyIndex) then begin
+                    if slTopLevelModPatternPaths.IndexOf(between) = -1 then begin
                         AddMessage('Added sub path: ' + between);
                         slTopLevelModPatternPaths.Add(between);
                     end;
@@ -5489,11 +5488,11 @@ begin
         //meshes\ to meshes\lod
         Result := StringReplace(f, parts[0], parts[0] + '\lod', [rfIgnoreCase]);
     end
-    else if slTopLevelModPatternPaths.Find(LowerCase(parts[1]), dummyIndex) then begin
+    else if slTopLevelModPatternPaths.IndexOf(LowerCase(parts[1])) <> -1 then begin
         //meshes\dlc03\path\to\model.nif to meshes\dlc03\lod\path\to\model.nif
         Result := StringReplace(f, parts[1], parts[1] + '\lod', [rfIgnoreCase]);
     end
-    else if slTopLevelModPatternPaths.Find(LowerCase(parts[1]) + '\', dummyIndex) then begin
+    else if slTopLevelModPatternPaths.IndexOf(LowerCase(parts[1]) + '\') <> -1 then begin
         //meshes\dlc03\path\to\model.nif to meshes\dlc03\lod\path\to\model.nif
         Result := StringReplace(f, parts[1] + '\', parts[1] + '\lod\', [rfIgnoreCase]);
     end
@@ -5754,7 +5753,7 @@ begin
             o := OverrideByIndex(masterRecord, i);
             f := GetFile(o);
             overrideFileName := GetFileName(f);
-            if slMasterableMasters.Find(overrideFileName, dummyIndex) then begin
+            if slMasterableMasters.IndexOf(overrideFileName) <> -1 then begin
                 Result := o;
                 Exit;
             end;
@@ -5763,7 +5762,7 @@ begin
     //If we got this far, this means the record either 1) Only exists in the master file or 2) None of the overrides are able to be placed in the inputFile.
     f := GetFile(masterRecord);
     masterFileName := GetFileName(f);
-    if slMasterableMasters.Find(masterFileName, dummyIndex) then begin
+    if slMasterableMasters.IndexOf(masterFileName) <> -1 then begin
         Result := masterRecord;
         Exit;
     end;
@@ -5803,7 +5802,7 @@ var
 begin
     Result := False;
     fileName := GetFileName(GetFile(WinningOverride(r)));
-    if slMasterableMasters.Find(fileName, dummyIndex) then Result := True;
+    if slMasterableMasters.IndexOf(fileName) <> -1 then Result := True;
 end;
 
 function CanOverrideDeterminesPlugin(r: IwbMainRecord; inputFile: IwbFile): IwbFile;
@@ -5829,7 +5828,7 @@ begin
     try
         for i := 0 to Pred(slMasterList.Count) do begin
             pluginFile := slMasterList[i];
-            if slListToSort.Find(pluginFile, dummyIndex) then temp.Add(pluginFile);
+            if slListToSort.IndexOf(pluginFile) <> -1 then temp.Add(pluginFile);
         end;
 
         slListToSort.Assign(temp);
